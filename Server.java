08c70bd8d70b64390284f1a4c9bd112859c93d33
altanab <badmalta@gmail.com>
2017-10-14 16:58:58 +0300

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,37 @@
+package test2;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+
+  public static void main(String[] args) throws IOException {
+	  
+    try (ServerSocket servers = new ServerSocket(4443)){  //some port for comp
+   
+    	System.out.print("Waiting for a client...");
+         Socket fromclient= servers.accept();
+         System.out.println("Client connected");
+   /////////////////     
+         DataOutputStream out = new DataOutputStream(fromclient.getOutputStream());
+         DataInputStream in = new DataInputStream(fromclient.getInputStream());
+         String input = in.readUTF();
+         //process data base -> output
+         String output = null;
+         out.writeUTF(output);
+         out.flush();
+         out.close();
+         in.close();
+         fromclient.close();
+    ////////////////
+         
+         //     servers.close();
+    } catch (IOException e) {
+      System.out.println("error");
+      System.exit(-1);
+    }
+
+   
+    
+  }
+}
