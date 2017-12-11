206850098e6ed00e78c74e71325c54c4bbf5a5cf
altanab <badmalta@gmail.com>
2017-12-11 18:16:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ var server = http.createServer(
 		
         	var query = "SELECT Flights.id, Flights.date_flight, Flights.cost, Flights.time_flight, C1.Country AS country_from, Cit1.City AS city_from, C2.Country AS country_to, Cit2.City AS city_to " + 
 			"FROM Flights, Countries AS C1, Cities AS Cit1, Countries AS C2, Cities AS Cit2 WHERE (Cit1.Country=C1.ID and Flights.city_to=Cit1.ID) AND (Cit2.Country=C2.ID and Flights.city_from=Cit2.ID)"
-        		+ "AND Cit1.City='" + from + "' AND Cit2.City='" + where + "' AND Flights.date_flight ='" + when + "'"
+        		+ "AND Cit1.City='" + from + "' AND Cit2.City='" + where + "' AND Flights.date_flight ='" + when + "'";
         	client.query(query, (err, res) => {
-        	if (err)
+        		if (err)
 				throw err;
 			var message = '', str = '';
         		for (let row of res.rows)
