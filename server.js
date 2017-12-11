eca1aa9bd2ec2e0289a40e55369230ba84043574
Polly-99 <prasnach@mail.ru>
2017-12-11 20:10:23 +0300

    new

--- server.js
+++ server.js
@@ -25,47 +25,49 @@ var server = http.createServer(
 		var from = '', where = '', when = '0001-01-01';
 		if (!input)
 			response.end(JSON.stringify("No request"));
-		JSON.parse(input, function(key, value) {
- 			if (key == 'from') 
-				from = value;
- 			if (key == 'where') 
-				where = value;
-			if (key == 'when') 
-				when = value;
-		});
+		else 
+		{
+			JSON.parse(input, function(key, value) {
+ 				if (key == 'from') 
+					from = value;
+ 				if (key == 'where') 
+					where = value;
+				if (key == 'when') 
+					when = value;
+			});
+			input = '{"from":"Moscow", "where":"Paris", "when":"2018-01-01"}';
+			var pg = require('pg');
+        		var conString = "postgres://postgres:postgres@localhost:5432/Test";
 	
-		const { Client } = require('pg');
-		const client = new Client({
- 			connectionString: process.env.DATABASE_URL,
-  			ssl: true,
-		});
-		client.connect();
+	     	  	 var client = new pg.Client(conString);
+        		client.connect();
 		
-        	var query = "SELECT Flights.id, Flights.date_flight, Flights.cost, Flights.time_flight, C1.Country AS country_from, Cit1.City AS city_from, C2.Country AS country_to, Cit2.City AS city_to " + 
-			"FROM Flights, Countries AS C1, Cities AS Cit1, Countries AS C2, Cities AS Cit2 WHERE (Cit1.Country=C1.ID and Flights.city_to=Cit1.ID) AND (Cit2.Country=C2.ID and Flights.city_from=Cit2.ID)"
-        		+ "AND Cit1.City='" + from + "' AND Cit2.City='" + where + "' AND Flights.date_flight ='" + when + "'";
-        	client.query(query, (err, res) => {
-        		if (err)
-				throw err;
-			var message = '', str = '';
-        		for (let row of res.rows)
-			{
-        			str = JSON.stringify(row);
-				message += str + ", ";
-			}
-			if (!message)
-				message = JSON.stringify("No tickets!");
-			else
-				message = '[' + message.substring(0, message.length - 2) + ']';
-			var shapka = JSON.stringify("HTTP/1.1 200 OK\r\n" +
- -                    "Server: YarServer/2009-09-09\r\n" +
- -                    "Content-Type: text/html\r\n" +
- -                    "Content-Length: " + message.length + "\r\n" +
- -                    "Connection: close\r\n\r\n");
- -			message = shapka + message;
-			client.end();
-       			response.end(message);	
-       		 });
+        		var query = "SELECT Flights.id, Flights.date_flight, Flights.cost, Flights.time_flight, C1.Country AS country_from, Cit1.City AS city_from, C2.Country AS country_to, Cit2.City AS city_to " + 
+				"FROM Flights, Countries AS C1, Cities AS Cit1, Countries AS C2, Cities AS Cit2 WHERE (Cit1.Country=C1.ID and Flights.city_to=Cit1.ID) AND (Cit2.Country=C2.ID and Flights.city_from=Cit2.ID)"
+        			+ "AND Cit1.City='" + from + "' AND Cit2.City='" + where + "' AND Flights.date_flight ='" + when + "'";
+        		client.query(query, (err, res) => {
+        			if (err)
+					throw err;
+				var message = '', str = '';
+        			for (let row of res.rows)
+				{
+        				str = JSON.stringify(row);
+					message += str + ", ";
+				}
+				if (!message)
+					message = JSON.stringify("No tickets!");
+				else
+					message = '[' + message.substring(0, message.length - 2) + ']';
+				var shapka = JSON.stringify("HTTP/1.1 200 OK\r\n" +
+                    			"Server: YarServer/2009-09-09\r\n" +
+                    			"Content-Type: text/html\r\n" +
+                     			"Content-Length: " + message.length + "\r\n" +
+                     			"Connection: close\r\n\r\n");
+ 				message = shapka + message;
+				client.end();
+       				response.end(message);
+       			});
+		}
 	});
 })
 server.listen(port);
